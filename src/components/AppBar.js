4bb9205959ed20b637673a34d992719b1e9a29af
Shajin <shajink4u@gmail.com>
2021-07-29 23:35:03 +0300

    fix the alignment

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -32,6 +32,7 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    marginBottom: theme.spacing(1),
   },
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
